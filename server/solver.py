82148d7f20586ef6b356e88c0815e866702ad1e8
Jeremy La <jeremylai511@gmail.com>
2024-05-30 22:43:35 -0400

    Added brute force and greedy TSP solutions

---
+++ server/solver.py
@@ -0,0 +1,36 @@
+from typing import List
+import math
+from itertools import permutations
+
+def get_distance(cities, distances, city1, city2):
+    """Helper function to find the distance between two cities given distances array."""
+    return distances[cities.index(city1)][cities.index(city2)]
+
+
+def brute_force(cities, distances):
+    """Brute force solution comparing all permutations of cities. Not recommended for more than 7-8 cities."""
+    perms = permutations(cities)
+    bestPerm = None
+    bestDistance = float('inf')
+    for perm in list(perms):
+        distance = 0
+        for i in range(len(perm) - 1):
+            distance += get_distance(cities, distances, perm[i], perm[i + 1])
+        if distance < bestDistance:
+            bestPerm = perm
+            bestDistance = bestDistance
+    return bestPerm
+
+
+def nearest_neighbor(cities: List[str], distances: List[List[int]]) -> List[str]:
+    """Suboptimal greedy solution."""
+    unvisited = set(cities)
+    current = cities[0]
+    unvisited.remove(current)
+    tour = [current]
+    while unvisited:
+        next_city = min(unvisited, key=lambda city: distances[cities.index(current)][cities.index(city)])
+        tour.append(next_city)
+        unvisited.remove(next_city)
+        current = next_city
+    return tour
---
+++ server/test_solver.py
@@ -0,0 +1,27 @@
+from solver import *
+
+def main():
+    cities = ["Augsburg", "Munich", "Stuttgart", "Nuremberg", "Leipzig", "Dresden", "Berlin", "Hanover", "Bremen", "Hamburg", "Cologne", "Frankfurt"]
+    citiesSmall = ["Augsburg", "Munich", "Stuttgart", "Nuremberg", "Leipzig", "Dresden", "Berlin"]
+
+    distances = [[0,57,133,122,347,362,495,452,543,578,404,250],    # Augsburg
+                [57,0,188,144,356,355,498,483,577,605,449,298],     # Munich
+                [133,188,0,157,365,412,511,402,480,534,289,153],    # Stuttgart
+                [122,144,157,0,225,258,374,338,433,461,337,187],    # Nuremberg
+                [347,356,365,225,0,99,150,215,311,295,380,294],     # Leipzig
+                [362,355,412,258,99,0,165,312,406,380,473,372],     # Dresden
+                [495,498,511,374,150,165,0,250,316,255,477,422],    # Berlin
+                [452,483,402,338,215,312,250,0,100,135,245,257],    # Hanover
+                [543,577,480,433,311,406,316,100,0,95,270,330],     # Bremen
+                [578,605,534,461,295,380,255,135,95,0,355,392],     # Hamburg
+                [404,449,289,337,380,473,477,245,270,355,0,150],    # Cologne 
+                [250,298,153,187,294,372,422,257,330,392,150,0]     # Frankfurt
+                ]
+    
+    print(nearest_neighbor(cities, distances))
+    print(brute_force(citiesSmall, distances))
+    print(nearest_neighbor(citiesSmall, distances))
+
+
+if __name__ == "__main__":
+    main()

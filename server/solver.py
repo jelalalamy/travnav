8a42968fdb180821eb46c3031650bc2c8b7cce6a
Jeremy La <jeremylai511@gmail.com>
2024-05-31 19:01:24 -0400

    Added rotate_list helper

--- server/solver.py
+++ server/solver.py
@@ -2,9 +2,16 @@ from typing import List
 from itertools import permutations
 
 def get_distance(cities, distances, city1, city2):
-    """Helper function to find the distance between two cities given distances array."""
+    """Helper function to find the distance between two cities given distances list."""
     return distances[cities.index(city1)][cities.index(city2)]
 
+
+def rotate_list(first, cities):
+    """Helper function to rotate a list so that first is the first element in the list."""
+    i = cities.index(first)
+    for _ in range(i):
+        cities.append(cities.pop(0))
+    
 
 def brute_force_solve(cities: List[str], distances: List[List[int]]) -> List[str]:
     """Brute force solution comparing all permutations of cities. Not recommended for more than 7-8 cities."""
@@ -17,8 +24,11 @@ def brute_force_solve(cities: List[str], distances: List[List[int]]) -> List[str
             distance += get_distance(cities, distances, perm[i], perm[i + 1])
         if distance < bestDistance:
             bestPerm = perm
-            bestDistance = bestDistance
-    return bestPerm
+            bestDistance = distance
+    # print(bestDistance)
+    best = list(bestPerm)
+    rotate_list(cities[0], best)
+    return best
 
 
 def nearest_neighbor_solve(cities: List[str], distances: List[List[int]]) -> List[str]:

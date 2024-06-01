77fe649a2bf4aff1257611860bd0113bb0913d04
Jeremy La <jeremylai511@gmail.com>
2024-05-31 21:50:00 -0400

    Successful request from client to server

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,30 @@
-function App() {
+import { useState } from "react";
+
+const App = () => {
+  const [data, setData] = useState("");
+
+  const onClickHandler = async () => {
+    const cities = ["Toronto", "Scarborough", "Markham", "North York"];
+    const distances = [[0, 10, 15, 20], 
+                    [10, 0, 35, 25], 
+                    [15, 35, 0, 30], 
+                    [20, 25, 30, 0]];
+    
+    const res = await fetch('http://localhost:5000/hello');
+    const resData = await res.json();
+    console.log(resData)
+
+    setData("asdf");
+  };
+
   return (
-    <h1 className="text-4xl font-bold underline">
-      Hello world!
-    </h1>
+    <div>
+      <h1 className="text-4xl font-bold underline">
+        Hello world!
+      </h1>
+      <button onClick={onClickHandler}>Click me</button>
+      <h3>{data}</h3>
+    </div>
   );
 }
 
--- server/app.py
+++ server/app.py
@@ -1,7 +1,9 @@
 from flask import Flask, request
+from flask_cors import CORS, cross_origin
 from solver import brute_force_solve, nearest_neighbor_solve
 
 app = Flask(__name__)
+CORS(app)
 # Set-ExecutionPolicy Unrestricted -Scope Process
 # .venv\Scripts\activate
 # flask --debug run
@@ -9,6 +11,10 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return '<p>Hello, World</p>'
+
+@app.get('/hello')
+def hello():
+    return {'data': 'hello'}
 
 @app.post('/brute')
 def brute_force():

19021cbeecea76d1156b0f88351734da3463d103
Jeremy La <jeremylai511@gmail.com>
2024-06-04 00:02:50 -0400

    Add Google Maps example

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,22 +1,23 @@
 import { useState } from "react";
+import { APIProvider, Map } from '@vis.gl/react-google-maps';
 
 const App = () => {
   const [data, setData] = useState([]);
 
   const onClickHandler = async () => {
     const cities = ["Toronto", "Scarborough", "Markham", "North York"];
-    const distances = [[0, 10, 15, 20], 
-                    [10, 0, 35, 25], 
-                    [15, 35, 0, 30], 
-                    [20, 25, 30, 0]];
-    
+    const distances = [[0, 10, 15, 20],
+    [10, 0, 35, 25],
+    [15, 35, 0, 30],
+    [20, 25, 30, 0]];
+
     const res = await fetch('http://localhost:5000/brute', {
       method: 'POST',
       headers: {
         'Accept': 'application/json',
         'Content-Type': 'application/json'
       },
-      body: JSON.stringify({cities: cities, distances: distances})
+      body: JSON.stringify({ cities: cities, distances: distances })
     });
     const resData = await res.json();
     console.log(resData)
@@ -25,9 +26,17 @@ const App = () => {
   };
 
   return (
-    <div>
+    <div className="h-screen">
       <button onClick={onClickHandler}>Click me</button>
       <h3>{data}</h3>
+      <APIProvider apiKey={process.env.REACT_APP_GMK as string}>
+        <Map
+          defaultZoom={3}
+          defaultCenter={{ lat: 22.54992, lng: 0 }}
+          gestureHandling={'greedy'}
+          disableDefaultUI={true}
+        />
+      </APIProvider>
     </div>
   );
 }

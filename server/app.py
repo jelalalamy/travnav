897ab2a9762eb748517fc6a160a64395094feae1
Jeremy La <jeremylai511@gmail.com>
2024-07-03 16:26:57 -0400

    Dockerize server

--- server/app.py
+++ server/app.py
@@ -11,7 +11,7 @@ CORS(app)
 
 @app.get('/hello')
 def hello():
-    return {'data': {'path': ['hello'], 'msg': 'hello'}}
+    return {'data': {'path': ['hellooo'], 'msg': 'hellooo'}}
 
 @app.post('/bestpath')
 def handler():
